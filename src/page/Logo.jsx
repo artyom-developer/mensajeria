10b3d3d155f48dc193c8ae1be570a34bb3206cea
artyom-developer <felipeperdomo32@hotmail.com>
2024-05-30 14:43:09 -0500

    first commit

---
+++ src/page/Logo.jsx
@@ -0,0 +1,9 @@
+import React from 'react';
+import { ReactComponent as MySvg } from './logoipsum-325.svg';
+
+export default function Logo () {
+  return (
+    <MySvg width="100" height="100" /> 
+  );
+};
+ 
---
+++ src/page/Nav.jsx
@@ -0,0 +1,42 @@
+import React from 'react';
+
+import Logo  from './Logo';
+
+function Nav({setMenu}) {
+  return ( 
+    <nav className="container p-6 mx-auto lg:flex lg:justify-between lg:items-center">
+        <div className="flex items-center justify-between">
+            <a href="#">
+                <Logo /> 
+            </a>
+        
+            <div className="flex lg:hidden">
+                <button type="button" className="text-gray-500 dark:text-gray-200 hover:text-gray-600 dark:hover:text-gray-400 focus:outline-none focus:text-gray-600 dark:focus:text-gray-400" aria-label="toggle menu">
+                    <svg   xmlns="http://www.w3.org/2000/svg" className="w-6 h-6" fill="none" viewBox="0 0 24 24" stroke="currentColor" stroke-width="2">
+                        <path stroke-linecap="round" stroke-linejoin="round" d="M4 8h16M4 16h16" />
+                    </svg>
+
+                    <svg x-show="isOpen" xmlns="http://www.w3.org/2000/svg" className="w-6 h-6" fill="none" viewBox="0 0 24 24" stroke="currentColor" stroke-width="2">
+                        <path stroke-linecap="round" stroke-linejoin="round" d="M6 18L18 6M6 6l12 12" />
+                    </svg>
+                </button>
+            </div>
+        </div>
+
+        <div className="absolute inset-x-0 z-20 w-full px-6 py-4 transition-all duration-300 ease-in-out bg-white shadow-md lg:bg-transparent lg:dark:bg-transparent lg:shadow-none dark:bg-gray-900 lg:mt-0 lg:p-0 lg:top-0 lg:relative lg:w-auto lg:opacity-100 lg:translate-x-0 lg:flex lg:items-center">
+            <div className="flex flex-col space-y-4 lg:mt-0 lg:flex-row lg:space-y-0">
+                <a className="text-gray-700 lg:mx-6 dark:text-gray-200 dark:hover:text-blue-400 hover:text-blue-500" href="#" onClick={()=>setMenu(1)}>Home</a> 
+                <a className="text-gray-700 lg:mx-6 dark:text-gray-200 dark:hover:text-blue-400 hover:text-blue-500" href="#"  onClick={()=>setMenu(2)}>Precios</a>
+                <a className="text-gray-700 lg:mx-6 dark:text-gray-200 dark:hover:text-blue-400 hover:text-blue-500" href="#"  onClick={()=>setMenu(4)}>FAQ</a>
+                <a className="text-gray-700 lg:mx-6 dark:text-gray-200 dark:hover:text-blue-400 hover:text-blue-500" href="#"  onClick={()=>setMenu(3)}>Contacto</a>
+            </div>
+
+            <a className="block h-10 px-5 py-2 mt-4 text-sm text-center text-gray-700 capitalize transition-colors duration-300 transform border rounded-md dark:hover:bg-gray-700 dark:text-white lg:mt-0 hover:bg-gray-100 lg:w-auto" href="#">
+                Registrar
+            </a>
+        </div>
+    </nav>
+  );
+}
+
+export default Nav;
