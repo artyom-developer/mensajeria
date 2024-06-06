50bc865c073c7d4b59794705bce8659599cdd14a
felipe-murcia <felipeperdomo32@hotmail.com>
2024-06-05 19:58:16 -0500

    add image for readme

--- src/App.js
+++ src/App.js
@@ -103,21 +103,18 @@ function App() {
                 <span class="inline-block w-3 h-1 mx-1 bg-blue-500 rounded-full"></span>
                 <span class="inline-block w-1 h-1 bg-blue-500 rounded-full"></span>
             </div>
-
-            <p class="max-w-2xl mx-auto mt-6 text-center text-gray-500 dark:text-gray-300">
-                Lorem ipsum dolor sit amet consectetur adipisicing elit. Illo incidunt ex placeat modi magni quia error alias, adipisci rem similique, at omnis eligendi optio eos harum.
-            </p>
+ 
         </div>
     </div>
 
     <div class="container px-6 py-10 mx-auto -mt-72 sm:-mt-80 md:-mt-96">
         <div class="grid grid-cols-1 gap-8 mt-8 xl:mt-16 md:grid-cols-2 xl:grid-cols-3">
             <div class="flex flex-col items-center p-4 border sm:p-6 rounded-xl dark:border-gray-700">
-                <img class="object-cover w-full rounded-xl aspect-square" src="https://images.unsplash.com/photo-1535713875002-d1d0cf377fde?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=880&q=80" alt="" />
+                <img class="object-cover w-full rounded-xl aspect-square"  src={require('./image/miguel.jpeg')} alt="" />
 
                 <h1 class="mt-4 text-2xl font-semibold text-gray-700 capitalize dark:text-white">Miguel Angel Moreno</h1>
 
-                <p class="mt-2 text-gray-500 capitalize dark:text-gray-300">design director</p>
+                <p class="mt-2 text-gray-500 capitalize dark:text-gray-300">Gerente General</p>
 
                 <div class="flex mt-3 -mx-2">
                     <a href="#" class="mx-2 text-gray-600 transition-colors duration-300 dark:text-gray-300 hover:text-blue-500 dark:hover:text-blue-400" aria-label="Reddit">
@@ -150,7 +147,7 @@ function App() {
             </div>
 
             <div class="flex flex-col items-center p-4 border sm:p-6 rounded-xl dark:border-gray-700">
-                <img class="object-cover w-full rounded-xl aspect-square" src="https://images.unsplash.com/photo-1531590878845-12627191e687?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=764&q=80" alt="" />
+                <img class="object-cover w-full rounded-xl aspect-square" src={require('./image/felipe.png')} alt="" />
 
                 <h1 class="mt-4 text-2xl font-semibold text-gray-700 capitalize dark:text-white">Luis Felipe Perdomo</h1>
 
@@ -187,7 +184,7 @@ function App() {
             </div>
 
             <div class="flex flex-col items-center p-4 border sm:p-6 rounded-xl dark:border-gray-700">
-                <img class="object-cover w-full rounded-xl aspect-square" src="https://images.unsplash.com/photo-1570295999919-56ceb5ecca61?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=880&q=80" alt="" />
+                <img class="object-cover w-full rounded-xl aspect-square" src={require('./image/sebas.jpeg')} alt="" />
 
                 <h1 class="mt-4 text-2xl font-semibold text-gray-700 capitalize dark:text-white">Juan Sebastian Murcia</h1>
 
